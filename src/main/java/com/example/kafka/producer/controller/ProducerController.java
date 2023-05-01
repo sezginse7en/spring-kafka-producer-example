3e5ba1294bc335ca99dd1a08d294996fb14e1217
sezginseven <sezgin_seven@windowslive.com>
2023-05-01 14:36:01 +0300

    first commit

---
+++ src/main/java/com/example/kafka/producer/controller/ProducerController.java
@@ -0,0 +1,36 @@
+package com.example.kafka.producer.controller;
+
+import com.example.kafka.producer.dto.Product;
+import com.example.kafka.producer.service.ProducerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("producer")
+public class ProducerController {
+
+    @Autowired
+    ProducerService producerService;
+
+    @GetMapping
+    public String healthCheck(@RequestBody String message){
+
+        producerService.send(message);
+        return "Message is sent succesfully";
+
+    }
+
+    @PostMapping
+    public Product sendProductMessage(@RequestBody Product message){
+
+        producerService.send(message);
+        return message;
+
+    }
+
+
+}
---
+++ src/main/java/com/example/kafka/producer/dto/Product.java
@@ -0,0 +1,11 @@
+package com.example.kafka.producer.dto;
+
+public class Product {
+
+    public String title;
+    public String description;
+    public String color;
+    public int price;
+    public boolean isInStock;
+
+}
